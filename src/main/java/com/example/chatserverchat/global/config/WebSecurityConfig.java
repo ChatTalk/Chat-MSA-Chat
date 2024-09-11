420feeec2ab7a4547fcfc9272b091c2387a0f981
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-11 16:29:51 +0900

    fix: 카프카 설정 재조정

---
+++ src/main/java/com/example/chatserverchat/global/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.chatserverchat.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // 카프카
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.sessionManagement(sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.authorizeHttpRequests(authorizeHttpRequests ->
+                authorizeHttpRequests
+                        .requestMatchers("/kafka/**").permitAll()
+                        .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+
+}
