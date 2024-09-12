fc49691316e71a2c29d4ad26d182f440e09c0d3e
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-12 17:18:00 +0900

    feat: 유저 인스턴스로 엑세스 토큰 전파를 위한 프로듀서 설정

---
+++ src/main/java/com/example/chatserverchat/global/config/KafkaProducerConfig.java
@@ -0,0 +1,36 @@
+package com.example.chatserverchat.global.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 유저 인스턴스에 엑세스 토큰을 전파하기 위한 프로듀서 설정
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${kafka.uri}")
+    private String uri;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, uri);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
--- src/main/java/com/example/chatserverchat/global/kafka/KafkaListenerService.java
+++ src/main/java/com/example/chatserverchat/global/kafka/KafkaListenerService.java
@@ -4,26 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-
-import static com.example.chatserverchat.global.constant.Constants.KAFKA_USER_DATA_KEY;
 import static com.example.chatserverchat.global.constant.Constants.KAFKA_USER_TO_CHAT_TOPIC;
 
 @Slf4j
 @Service
 public class KafkaListenerService {
 
-    private static final ConcurrentMap<String, String> emailData = new ConcurrentHashMap<>();
-
     // 필요한 다른 곳에서 동일한 어노테이션을 할당하고 메세지를 받으면 됨
     @KafkaListener(topics = KAFKA_USER_TO_CHAT_TOPIC, groupId = "user")
     public void listen(String message) {
         log.info("수신한 이메일: {} ", message);
-        emailData.putIfAbsent(KAFKA_USER_DATA_KEY, message);
-    }
-
-    public static String getEmail() {
-        return emailData.get(KAFKA_USER_DATA_KEY);
     }
 }
