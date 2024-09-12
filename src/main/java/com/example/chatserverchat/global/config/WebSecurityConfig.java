8f24ce71f3cb86da62855974f6a98c7020458d7a
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-12 15:02:40 +0900

    fix: 임시 시큐리티 설정 작성

--- src/main/java/com/example/chatserverchat/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/chatserverchat/global/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.chatserverchat.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,6 +26,9 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests(authorizeHttpRequests ->
                 authorizeHttpRequests
                         .requestMatchers("/kafka/**").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/api/open-chats").permitAll()
+                        // 임시 코드 추가, 얘를 추가하니 채팅창 조회가 작동됨
+                        // 하지만 얘를 추가하는 건 해결책이 아님. 인증이 유명무실해지는 거니까
                         .anyRequest().authenticated()
         );
 
