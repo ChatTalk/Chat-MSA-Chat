2af86e2ba5b01e8cc5cc5aabe5d35e4448ebf13c
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-13 01:23:07 +0900

    feat: 필터 설정 추가

--- src/main/java/com/example/chatserverchat/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/chatserverchat/global/config/WebSecurityConfig.java
@@ -1,17 +1,35 @@
 package com.example.chatserverchat.global.config;
 
+import com.example.chatserverchat.global.security.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final RedisTemplate<String, String> cacheTemplate;
+
+    public WebSecurityConfig(
+            UserDetailsService userDetailsService,
+            KafkaTemplate<String, String> kafkaTemplate,
+            @Qualifier("cacheTemplate") RedisTemplate<String, String> cacheTemplate) {
+        this.userDetailsService = userDetailsService;
+        this.kafkaTemplate = kafkaTemplate;
+        this.cacheTemplate = cacheTemplate;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,6 +49,10 @@ public class WebSecurityConfig {
                         // 하지만 얘를 추가하는 건 해결책이 아님. 인증이 유명무실해지는 거니까
                         .anyRequest().authenticated()
         );
+
+        http.addFilterBefore(new AuthenticationFilter(
+                userDetailsService, kafkaTemplate, cacheTemplate
+        ), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
