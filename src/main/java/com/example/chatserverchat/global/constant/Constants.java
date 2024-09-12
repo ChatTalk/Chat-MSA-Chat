b214b96881baea4e24ee826df2decab5f594e202
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-12 17:39:15 +0900

    feat: 토큰 추출 후, 유저 인스턴스로 토큰 전파

--- src/main/java/com/example/chatserverchat/global/constant/Constants.java
+++ src/main/java/com/example/chatserverchat/global/constant/Constants.java
@@ -9,9 +9,11 @@ public final class Constants {
     public static final String COOKIE_AUTH_HEADER = "Authorization";
     public static final String REDIS_REFRESH_KEY = "REFRESH_TOKEN:";
     public static final String REDIS_SUBSCRIBE_KEY = "SUBSCRIBE:";
+    public static final String BEARER_PREFIX = "Bearer ";
 
     // kafka 상수
     public static final String KAFKA_USER_TO_CHAT_TOPIC = "email";  // chat 인스턴스에 전파하기 위한 토픽
     public static final String KAFKA_USER_DATA_KEY = "username"; // 동시성 해시 맵의 key 값
     public static final String KAFKA_USER_TO_CHAT_GROUP_ID = "dev";
+    public static final String KAFKA_OTHER_TO_USER_TOPIC = "authorization";
 }
--- src/main/java/com/example/chatserverchat/global/security/AuthenticationFilter.java
+++ src/main/java/com/example/chatserverchat/global/security/AuthenticationFilter.java
@@ -3,23 +3,52 @@ package com.example.chatserverchat.global.security;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+import static com.example.chatserverchat.global.constant.Constants.*;
+
 @Slf4j
+@RequiredArgsConstructor
 public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // redis 캐시를 조회했을 때 없으면 그제서야 카프카를 통해 인증 정보인 이메일을 받아오게 된다.
+        // 쿠키에 담긴 엑세스 토큰을 받아와서 가져와야 한다.
+        String beforeToken = findAccessToken(request.getCookies());
+        log.info("초기 토큰값: {}", beforeToken);
 
+        /**
+         * 여기서 바로 카프카로 전송해야 되는 걸까?
+         */
+
+        if (beforeToken != null) {
+            kafkaTemplate.send(KAFKA_OTHER_TO_USER_TOPIC, beforeToken);
+        } else {
+            throw new IllegalArgumentException("엑세스 토큰이 존재하지 않음");
+        }
+
+        // 향후 로직... 인증 정보 받아오기...
 
     }
 
+    // 엑세스 토큰 찾기
+    private String findAccessToken(Cookie[] cookies){
+        if (cookies == null) return null;
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(COOKIE_AUTH_HEADER)) return cookie.getValue();
+        }
+        return null;
+    }
 }
