d2e686c17c56439ee02ce4b7fca86d3aec7665cb
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-16 13:44:00 +0900

    fix: 인증 결합도 감축 및 의존성 정리

--- src/main/java/com/example/chatserverchat/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/chatserverchat/global/config/WebSecurityConfig.java
@@ -34,16 +34,16 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        // 카프카
         http.csrf(AbstractHttpConfigurer::disable);
 
         http.sessionManagement(sessionManagement ->
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
 
+        http.formLogin(AbstractHttpConfigurer::disable);
+
         http.authorizeHttpRequests(authorizeHttpRequests ->
                 authorizeHttpRequests
-                        .requestMatchers("/kafka/**").permitAll()
 //                        .requestMatchers(HttpMethod.GET, "/api/open-chats").permitAll()
                         // 임시 코드 추가, 얘를 추가하니 채팅창 조회가 작동됨
                         // 하지만 얘를 추가하는 건 해결책이 아님. 인증이 유명무실해지는 거니까
