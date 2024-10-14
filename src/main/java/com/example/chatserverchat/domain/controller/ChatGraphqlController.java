37eabadd22af419976c7bcc5156f4e433af339d0
kimD0ngjun <chickentasty0112@gmail.com>
2024-10-14 15:32:07 +0900

    fix: 누락 어노테이션 할당

---
+++ src/main/java/com/example/chatserverchat/domain/controller/ChatGraphqlController.java
@@ -0,0 +1,39 @@
+package com.example.chatserverchat.domain.controller;
+
+import com.example.chatserverchat.domain.dto.ChatRoomDTO;
+import com.example.chatserverchat.domain.dto.GraphqlDTO;
+import com.example.chatserverchat.domain.entity.ChatRoom;
+import com.example.chatserverchat.domain.mapper.ChatRoomMapper;
+import com.example.chatserverchat.domain.repository.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/graphql")
+public class ChatGraphqlController {
+
+    private final ChatRoomRepository chatRoomRepository;
+
+    @QueryMapping
+    public GraphqlDTO getChatRoomById(@Argument String id) {
+        ChatRoom chatRoom =
+                chatRoomRepository.findById(Long.parseLong(id)).orElseThrow(null);
+
+        log.info("그래프큐엘 찾았냐: {}", chatRoom.getId());
+
+        return new GraphqlDTO(
+                chatRoom.getId().toString(),
+                chatRoom.getTitle(),
+                chatRoom.getOpenUsername(),
+                chatRoom.getMaxPersonnel(),
+                chatRoom.getCreatedAt().toString()
+        );
+    }
+
+}
---
+++ src/main/java/com/example/chatserverchat/domain/dto/GraphqlDTO.java
@@ -0,0 +1,4 @@
+package com.example.chatserverchat.domain.dto;
+
+public record GraphqlDTO(String id, String title, String openUsername, Integer maxPersonnel, String createdAt) {
+}
